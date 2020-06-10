393c4e089f2a5074d27232dd8980c0fcf3e898ad
Maxim Squ <fifthdouble@gmail.com>
2020-06-10 16:59:08 +0500

    Binaries del

---
+++ Source/Pitch_Fleck/MyClass.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
+#include "MyClass.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PITCH_FLECK_API UMyClass : public UWidgetComponent
+{
+	GENERATED_BODY()
+	
+};
